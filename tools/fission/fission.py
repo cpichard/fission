a39def9805ec249410bfe9a96c7a7d7030f1a123
cyril <pichard.cyril@laposte.net>
2013-04-14 15:24:55 +0100

    basic ui work in progress

--- tools/fission/fission.py
+++ tools/fission/fission.py
@@ -1,20 +1,21 @@
 
 import sys
-#
+from ctypes import CDLL, RTLD_GLOBAL 
 from PySide.QtCore import *
 from PySide.QtGui import *
 
-# Create a Qt application
-app = QApplication(sys.argv)
+from MainWindow import MainWindowWidget
 
-# Create a Label and show it
-label = QLabel("Hello World")
+def main():
+    #lib = cdll.LoadLibrary("libfission.so", mode=RTLD_GLOBAL)
+    lib = CDLL("libfission.so", mode=RTLD_GLOBAL)
+    lib.fission_init()
+    app = QApplication(sys.argv)
+    mainWindow = MainWindowWidget(lib)
+    mainWindow.show()
+    app.exec_()
+    lib.fission_exit()
+    sys.exit()
 
-label.show()
-#
-# Enter Qt application main loop
-#
-app.exec_()
-
-sys.exit()
-
+if __name__ == "__main__":
+    main()
