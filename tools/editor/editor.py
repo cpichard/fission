773b5f288ec3d52f6e831a38d3af352128b4bcbb
cyril <cyril@palourde-2.local>
2013-09-09 22:41:06 +0100

    minor changes for python bindings

--- tools/editor/editor.py
+++ tools/editor/editor.py
@@ -6,8 +6,12 @@ from PySide.QtGui import *
 from MainWindow import MainWindowWidget
 
 def main():
-    from ctypes import CDLL, RTLD_GLOBAL
-    lib = CDLL("libfission.so", RTLD_GLOBAL)
+    try: # only on linux, we need to make sure
+         # the library is correctly loaded for the jit
+        from ctypes import CDLL, RTLD_GLOBAL
+        lib = CDLL("libfission.so", RTLD_GLOBAL)
+    except:
+        pass
     app = QApplication(sys.argv)
     mainWindow = MainWindowWidget()
     mainWindow.show()
